d40b831019ee15522d01d667ac16f42a81ea347c
Didier Thomas <didierthomas.m@gmail.com>
2023-01-24 13:34:30 -0600

    Casi listo

--- package/module_client.py
+++ package/module_client.py
@@ -1,34 +1,62 @@
-from module_functions import *
+from package.module_functions import user_id
 
-class Client():
-    def __init__(self, fname, lname, age, email, address) -> None:
-        # def get_address(self) :
-            # address = {'calle': str, 'numero exterior': int, 'numero interior': '', 'colonia': str, 'ciudad': str, 'estado': str, 'pais': str}
-            # address['calle'] = input('Nombre de la calle: ')
-            # address['numero exterior'] = int(input('Numero exterior: '))
-            # address['numero interior'] = int(input('Numero exterior: '))
-            # address['colonia']
-            # return address
+
+class Address:
+    def __init__(self, calle, nexterior, colonia, cpostal, ciudad, estado, pais, ninterior=None) -> None:
+        self.calle = calle
+        self.nexterior = nexterior
+        self.ninterior = ninterior
+        self.colonia = colonia
+        self.cpostal = cpostal
+        self.ciudad = ciudad
+        self.estado = estado
+        self.pais = pais
+
+    def __str__(self):
+        return f'Calle {self.calle}'
+
+    def make_address(self) -> dict:
+        address = {
+            'calle': self.calle,
+            'numero exterior': self.nexterior,
+            'numero interior': self.ninterior,
+            'colonia': self.colonia,
+            'codigo postal': self.cpostal,
+            'ciudad': self.ciudad,
+            'estado': self.estado,
+            'pais': self.pais
+        }
+        return address
+
+
+class Client:
+    def __init__(self, fname, lname, age, email, calle, nexterior, colonia, cpostal, ciudad, estado, pais,
+                 ninterior=None) -> None:
         self._fname = fname
         self._lname = lname
         self._age = age
         self._email = email
-        self._address = address
-        self._client_number = client_number()
+        self._address = Address(calle, nexterior, colonia, cpostal, ciudad, estado, pais, ninterior)
+        self._client_number = user_id()
 
     def __str__(self) -> str:
         return f'El nombre del clientes es {self._fname} {self._lname}.'
 
+    def make_client(self) -> dict:
+        name = {
+            'nombre': self._fname,
+            'apellido': self._lname,
+            'edad': self._age,
+            'e-mail': self._email,
+            'direccion': self._address.make_address()
+        }
+        return name
+
     def legal_age(self) -> str:
-        if self._age >= 18:
+        if int(self._age) >= 18:
             return f'El cliente puede comprar cualquier artículo.'
         else:
             return f'Necesitas la autorización de un adulto.'
 
     def purchase(self, item):
-        return f'El artículo {item} será enviado a {self._address}, para mayor información revise su correp {self._email}.'
-
-
-
-
-
+        return f'El artículo {item} será enviado a {self._address}, para mayor información revise su correo {self._email}.'
--- package/module_functions.py
+++ package/module_functions.py
@@ -1,4 +1,54 @@
-import random
+import json
+import os
 
-def client_number():
-    return random.randint(100, 1000)
+folder_data = os.path.join(os.path.abspath('.'), 'data')
+clients_db = 'clients_db'
+
+
+# Carga la base de datos
+def load_db(db_file: str = 'clients_db'):
+    if db_file and not db_file == '':
+        users_db = db_file
+    else:
+        users_db = 'clients_db'
+
+    users = dict()
+    path_to_json = os.path.join(folder_data, users_db + '.json')
+
+    if os.path.exists(path_to_json):
+        users = json.load(open(path_to_json, 'r'))
+        
+    else:
+        print('La base de datos no existe.')
+        answer = input(f'¿Desear crear la base de datos {users_db} si/no?')
+        if answer.lower() == 'si':
+            json.dump({}, open(path_to_json, 'w'))
+            users = json.load(open(path_to_json, 'r'))
+    return users
+
+
+# Crea un número de usuario
+def user_id(db_file: str = 'clients_db'):
+    user = load_db(db_file)
+    if not user:
+        return 1
+    else:
+        return int(list(user.keys())[-1]) + 1
+
+
+# Guarda el cliente nuevo en la basae de datos
+def save_new_client(new_client, db_file: str = 'clients_db'):
+    cliente = new_client.make_client()
+    no_id = new_client._client_number
+    new_client = {no_id: cliente}
+
+    path_to_json = os.path.join(folder_data, db_file + '.json')
+
+    if new_client:
+        json.dump(new_client, open(path_to_json, 'w'), indent=4)
+        print('Usuario guardado')
+        print('')
+    else:
+        print('No había que guardar')
+
+# Regresa la edad
